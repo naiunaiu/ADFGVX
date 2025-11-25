ee626e44ba450b2eb657f742aaa07866b3d659d4
naiu <imagenaru2@gmail.com>
2025-11-25 15:42:55 +0900

    test.bashをもう11回修正

--- test.bash
+++ test.bash
@@ -16,8 +16,8 @@ cd test_sys
 
 normal_output_copy="'src/copu.txt'"
 normal_output_move="'src/movu.txt'"
-out_copy=$( ../fetch -c test_sys/src copu.txt)
-out_move=$( ../fetch -m test_sys/src movu.txt)
+out_copy=$( ../fetch -c src/ copu.txt)
+out_move=$( ../fetch -m src/ movu.txt)
 exit_code=$?
 
 [ "${out_copy}" = "${normal_output_copy}" ] || ng "$LINENO"
